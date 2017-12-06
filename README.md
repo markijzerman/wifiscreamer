2a4d590cc83c068394559e1d845b6dc2919a0911
Mark IJzerman <markijzerman@gmail.com>
2017-12-06 22:12:31 +0100

    test with pySSML

--- README.md
+++ README.md
@@ -4,5 +4,3 @@
 This python code is meant to run on a Raspberry Pi Zero W.
 It reads out nearby wifi networks and reads them out in the silliest voice Amazon's Polly has to offer.
 To do this, we use Amazon's AWS services.
-
-Bla
--- testspeech.py
+++ testspeech.py
@@ -1,16 +1,29 @@
 import boto3
 import subprocess
+from pyssml.PySSML import PySSML
+s = PySSML()
 session = boto3.Session(profile_name='default')
 polly = session.client('polly')
 
 # USE POLLY
 # Alle documentatie nodig: http://docs.aws.amazon.com/polly/latest/dg/supported-ssml.html
+# http://docs.aws.amazon.com/polly/latest/dg/voicelist.html
 # <speak><prosody rate="x-slow">Hallo <prosody pitch="+80%">Adriaan.</prosody></prosody><amazon:effect name="whispered">Ik heb een beetje binnensmond gekotst van geluk zonet.</amazon:effect><prosody pitch="+80%"><prosody rate="x-slow">doe mij maar een hele <break time="0.1s"/> dikke <break time="0.1s"/> <prosody pitch="-80%">HEMA-worst.</prosody></prosody></prosody></speak>
 
-text_to_speak = '<speak><prosody rate="x-slow">Hallo <prosody pitch="-80%">jongons.</prosody></prosody><amazon:effect name="whispered">.</amazon:effect><prosody pitch="+80%"><prosody rate="x-slow">doe mij maar een hele <break time="0.1s"/> dikke <break time="0.1s"/> <prosody pitch="-80%">HEMA-worst.</prosody></prosody></prosody></speak>'
-spoken_text = polly.synthesize_speech(Text=text_to_speak, 
+good_attributes = {'rate': 'x-slow', 'pitch': 'low', 'volume': 'soft'}
+bad_attribute = {'rate': 'slow', 'pitcher': 'high', 'volume': 'loud'}
+bad_attribute2 = {'rate': {}, 'pitch': 'high', 'volume': 'loud'}
+bad_value = {'rate': 'slow', 'pitch': 'Frank', 'volume': 'loud'}
+
+
+s.prosody(good_attributes, 'helicopter')
+s.say('add something')
+s.prosody(good_attributes, 'another thing')
+
+
+spoken_text = polly.synthesize_speech(Text=s.ssml(), 
 OutputFormat ='mp3', 
-VoiceId='Ruben', 
+VoiceId='Joey', 
 TextType='ssml')
 
 with open('output.mp3', 'wb') as f:
